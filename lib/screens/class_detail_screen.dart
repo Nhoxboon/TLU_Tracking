019141ec61b93b985fe6f39eed0ad7979d3b8bea
quangbach1709 <bachh1124@edumail365.onmicrosoft.com>
2025-09-24 15:56:25 +0700

    UI setting and edit project pass

--- lib/screens/class_detail_screen.dart
+++ lib/screens/class_detail_screen.dart
@@ -5,6 +5,7 @@ import '../models/teaching_session.dart';
 import 'add_session_screen.dart';
 import 'edit_session_screen.dart';
 import 'session_detail_screen.dart';
+import 'settings_screen.dart';
 
 class ClassDetailScreen extends StatefulWidget {
   final String classCode;
@@ -186,7 +187,12 @@ class _ClassDetailScreenState extends State<ClassDetailScreen> {
             IconButton(
               icon: const Icon(Icons.person_outline, size: 25),
               color: Colors.black.withOpacity(0.7),
-              onPressed: () {},
+              onPressed: () {
+                Navigator.push(
+                  context, 
+                  MaterialPageRoute(builder: (context) => const SettingsScreen()),
+                );
+              },
             ),
           ],
         ),
